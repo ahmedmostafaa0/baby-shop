997c8685a258137ef8d6d4273fef7d45bef7ecf6
ahmedmostafaa0 <a7med.mostafa.s3d@gmail.com>
2026-02-11 20:03:55 +0200

    Update access Token expires Time

--- server/utils/generateToken.ts
+++ server/utils/generateToken.ts
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 
 const generateTokens = (id: string) => {
     const accessToken = jwt.sign({id}, process.env.JWT_ACCESS_SECRET!, {
-        expiresIn: '1m'
+        expiresIn: '15m'
     })
     const refreshToken = jwt.sign({id}, process.env.JWT_REFRESH_SECRET!, {
         expiresIn: '30d'
